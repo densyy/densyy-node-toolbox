272c9f07ebca79deb05cbe95bf6a43a0f64700e6
Densyy <densyy@hotmail.com>
2025-02-27 18:13:44 -0300

    add tools/node-response

---
+++ core/tools/node-response.js
@@ -0,0 +1,38 @@
+import language from '../languages/pt-BR'
+
+const STATUS_OK = 'ok'
+const STATUS_ERROR = 'error'
+const STATUS_CODE_SUCCESS = 200
+const STATUS_CODE_CREATED = 201
+const STATUS_CODE_SERVER_ERROR = 500
+const SERVER_ERROR_MESSAGE = language.tools.response_1
+
+export default Object.freeze(
+  class NodeResponse {
+    success (res, message) {
+      return this._sendResponse(res, STATUS_OK, STATUS_CODE_SUCCESS, message)
+    }
+
+    create (res, message) {
+      return this._sendResponse(res, STATUS_OK, STATUS_CODE_CREATED, message)
+    }
+
+    simpleError (res, statusCode, message) {
+      return this._sendResponse(res, STATUS_ERROR, statusCode, message)
+    }
+
+    serverError (res) {
+      return this._sendResponse(res, STATUS_ERROR, STATUS_CODE_SERVER_ERROR, SERVER_ERROR_MESSAGE)
+    }
+
+    empty (res) {
+      return this._sendResponse(res, STATUS_OK, STATUS_CODE_SUCCESS, null)
+    }
+
+    _sendResponse (res, status, statusCode, body) {
+      return res
+        .status(statusCode)
+        .json({ status, statusCode, body })
+    }
+  }
+)
